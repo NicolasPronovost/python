d8e35cc4b4da73119556469419ed34fce61d651d
NicolasPronovost <nickprono04@gmail.com>
2023-10-08 11:19:30 -0400

    added seeker

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import user2
 
 def step1():
     print ("This is hide or die")
-
     choose = True
     while choose:
         player = input("Who do you want to play as: ")
@@ -13,14 +12,19 @@ def step1():
             choose = False
     return(player)
 
-def step2():
+def step2user1():
+    print ("Have have survied step 1 now chose your hiding sport")
     choose = True
     while choose:
-        hidingspot = input("Where do you want to hid: ")
-        if hidingspot == "spot1" or hidingspot == "spot2":
-            print("you will be hiding at", hidingspot)
+        hidingspot = input ("where are you going to hid?: ")
+        if hidingspot == "user1HidingSpot1" or hidingspot == "user1HidingSpot2":
+            print ("You will be hiding at", hidingspot, "good luck")
             choose = False
-    return(player)
+    return(hidingspot)
 
-   
-
+def seeker():
+    diceroll = random.randint(1,2)
+    if diceroll == 1:
+       print("the seeker has chosen to go and investiagte spot 1")
+    else:
+        print("the seeker is going to investigate spot 2")
--- game.py
+++ game.py
@@ -9,9 +9,21 @@ if player == "user1":
 else:
     user2.user2hiding()    
 
-hidingspot=app.step2()
-if hidingspot == "spot1":
-    user1.user1HidingSpot1
-else:
-    user1.user1HidingSpot2
+if player == "user1":
+    hiding = input("where do you want to hid: ")
+    print ("you will be hiding in", hiding)
+    if hiding == "spot1":
+        user1.user1HidingSpot1()
+    else:
+        user1.user1HidingSpot2()
+
+if player == "user2":
+    hiding = input("where do you want to hid: ")
+    print (player, "you will be hiding in", hiding)
+    if hiding == "spot1":
+        user2.user2HidingSpot1()
+    else:
+        user2.user2HidingSpot2()
+
+print ("the seeker will now chose if he will be going to spot 1 or spot 2 good luck")
 
